a9bbc5a8c32a0486a33d66a586b95e7f6a4853f4
Kevin Hsieh <kevin.b.hsieh@gmail.com>
2018-05-04 10:28:55 -0400

    problem 2 completed

--- java/src/main/java/com/euler/Main.java
+++ java/src/main/java/com/euler/Main.java
@@ -1,6 +1,7 @@
 package com.euler;
 
-import com.euler.problems.*;
+import com.euler.problems.Problem1;
+import com.euler.problems.Problem2;
 
 public class Main {
 	
@@ -10,6 +11,9 @@ public class Main {
 	public static void main(String[] args) {
 		Problem1 p1 = new Problem1();
 		System.out.println(p1.SumOf3or5());
+		
+		Problem2 p2 = new Problem2();
+		System.out.println(p2.sumOfEvens());
 	}
 
 }
